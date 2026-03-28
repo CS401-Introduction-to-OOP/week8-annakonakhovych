2e64267206853caf0883dbf2ca8710bae529f074
annakonakhovych <anna.konakhovych.25@kse.org.ua>
2026-03-28 15:24:09 +0200

    Update Program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace Generics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Letter l1 = new Letter("1A", 0.5);
+        Letter l2 = new Letter("2B", 0.3);
+        Parcel p1 = new Parcel("CB", 10, "20*10*30");
+        Parcel p2 = new Parcel("KM", 23.4, "40*30*50");
+
+        l1.PrintInfo();
+        p2.PrintInfo();
+
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem> ();
+        myCargo.AddItem(l1);
+        myCargo.AddItem(l2);
+        myCargo.AddItem(p1);
+        myCargo.AddItem(p2);
+        
+        Console.WriteLine($"Total sum of all your sendings is {myCargo.GetTotalCost()} uah");
+
+        
+        
+
+
+    }
+}
